fc4c588b1475af8e17da891e1a30e402adaa6da7
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-10-22 16:04:18 -0400

    discard cpp17 standard; use Boost.Filesystem

--- lab6/src/lab6_main.cpp
+++ lab6/src/lab6_main.cpp
@@ -1,15 +1,18 @@
+#include <ros/package.h>
 #include <ros/ros.h>
 
-#include <filesystem>
+#include <boost/filesystem.hpp>
 #include <lab6/pure_pursuit.hpp>
 
-namespace fs = std::filesystem;
+namespace fs = boost::filesystem;
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "pure_pursuit_planner", ros::InitOption::AnonymousName);
 
+  fs::path package_path = ros::package::getPath("lab6");
+
   lab6::PurePursuitPlanner planner(
-      fs::current_path() / fs::path("src/F1Tenth_Labs/lab6/csv/levine_pf.csv"));
+      (package_path / fs::path("csv/levine_pf.csv")).string());
 
   ros::spin();
   return 0;
